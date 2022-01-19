9d24807dfe1775e316c8084e2e68e1a4ca749269
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2022-01-19 15:01:17 -0500

    added Hero Button to home page

--- ICE-2/Scripts/app.js
+++ ICE-2/Scripts/app.js
@@ -2,9 +2,29 @@
 // AKA Anonymous Self-Executing Function
 (function()
 {
+
+    function DisplayHomePage()
+    {
+        let AboutUsButton = document.getElementById("AboutUsButton");
+        
+        AboutUsButton.addEventListener("click", function()
+        {
+            // redirect to about page
+            location.href = "about.html";
+        });
+    }
+
+    // named function
     function Start()
     {
         console.log("App Started!!");
+
+        switch(document.title)
+        {
+            case "Home":
+                DisplayHomePage();
+                break;
+        }
     }
 
     window.addEventListener("load", Start);
