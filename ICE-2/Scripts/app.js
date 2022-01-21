2b1ebab046aef604a012264450d7e9563bebbd52
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2022-01-21 15:14:08 -0500

    added DOM Insertion to index.html

--- ICE-2/Scripts/app.js
+++ ICE-2/Scripts/app.js
@@ -12,6 +12,30 @@
             // redirect to about page
             location.href = "about.html";
         });
+
+        // Step 1 get a reference to an entry point(s) (insertion point / deletion point)
+        let MainContent = document.getElementsByTagName("main")[0];
+        let DocumentBody = document.body;
+        
+        // Step 2 create an element(s) to insert
+        let MainParagraph = document.createElement("p");
+        let Article = document.createElement("article");
+        let ArticleParagraph = `<p id="ArticleParagraph" class="mt-3">This is the Article Paragraph</p>`;
+
+        // Step 3 configure new element
+        MainParagraph.setAttribute("id", "MainParagraph");
+        MainParagraph.setAttribute("class", "mt-3");
+        MainParagraph.textContent = "This is the Main Paragraph";
+        Article.setAttribute("class", "container");
+
+        // Step 4 add / insert new element
+        MainContent.appendChild(MainParagraph);
+        Article.innerHTML = ArticleParagraph;
+        DocumentBody.appendChild(Article);
+
+        
+
+
     }
 
     // named function
