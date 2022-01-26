33bdbc49bb3e881c6d0cdc902a630574a47f96cc
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2022-01-26 14:55:10 -0500

    added Contact class

---
+++ ICE-3/Scripts/contact.js
@@ -0,0 +1,49 @@
+class Contact
+{
+    // public properties (getters / setters)
+    get FullName()
+    {
+        return this.m_fullName;
+    }
+
+    set FullName(fullName)
+    {
+        this.m_fullName = fullName;
+    }
+
+    get ContactNumber()
+    {
+        return this.m_contactNumber;
+    }
+
+    set ContactNumber(contactNumber)
+    {
+        this.m_contactNumber = contactNumber;
+    }
+
+    get EmailAddress()
+    {
+        return this.m_emailAddress;
+    }
+
+    set EmailAddress(emailAddress)
+    {
+        this.m_emailAddress = emailAddress;
+    }
+
+    // constructor
+    constructor(fullName, contactNumber, emailAddress)
+    {
+        this.FullName =fullName;
+        this.ContactNumber = contactNumber;
+        this.EmailAddress = emailAddress;
+    }
+
+    // public methods
+
+    // overridden methods
+    toString()
+    {
+        return `Full Name: ${this.FullName}\n Contact Number: ${this.ContactNumber}\n Email Address: ${this.EmailAddress}`;
+    }
+}
