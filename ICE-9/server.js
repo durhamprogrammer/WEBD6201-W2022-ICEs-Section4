504552c6dd2077497671d4e54ba9e864d8d02ffa
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2022-03-16 14:42:05 -0400

    converted code to short form

--- ICE-9/server.js
+++ ICE-9/server.js
@@ -2,16 +2,5 @@ const hello = require('./hello');
 const users = require('./users.data');
 
 users.getData()
-.then(function(data)
-{
-    hello.sayHello();
-    console.log(data);
-    hello.sayGoodbye();
-})
-.catch(function(err){
-    console.error("ERROR: User Data Failed to Load");
-});
-
-
-
-
+.then((data) => { hello.sayHello(); console.log(data); hello.sayGoodbye(); })
+.catch((err) => { console.error("ERROR: User Data Failed to Load: " + err.message); });
--- ICE-9/users.data.js
+++ ICE-9/users.data.js
@@ -1,14 +1,4 @@
 const fs = require('fs');
 const fsPromises = fs.promises; // alias
 
-module.exports = {
-    getData: async function()
-    {
-        return await fsPromises.readFile("./Data/users.json", "utf8", function(err, data)
-        {
-           return data;
-        });
-    } 
-}
-
-
+module.exports.getData = async () => await fsPromises.readFile("./Data/users.json", "utf8", (err, data) => data);
