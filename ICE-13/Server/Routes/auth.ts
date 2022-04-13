07ae388a6c2a799ddcae91b28416cbc2c847feb1
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2022-04-13 14:07:10 -0400

    created auth controller

--- ICE-13/Server/Routes/auth.ts
+++ ICE-13/Server/Routes/auth.ts
@@ -1,106 +1,23 @@
 import express from 'express';
 const router = express.Router();
-import passport from 'passport';
 
-import User from '../Models/user';
-import { UserDisplayName } from '../Util/index';
+// controller instance
+import { DisplayLoginPage, DisplayRegisterPage, ProcessLoginPage, ProcessLogoutPage, ProcessRegisterPage } from '../Controllers/auth';
 
 /*************************************** AUTHENTICATION ROUTES************************************************/
 /* GET - Display login page. */
-router.get('/login', function(req, res, next) 
-{
-  if(!req.user)
-  {
-    return res.render('index', 
-      { title: 'Login', page: 'login', messages: req.flash('loginMessage'), displayName: UserDisplayName(req) });
-  }
-  return res.redirect('/contact-list');
-});
+router.get('/login', DisplayLoginPage);
 
 /* Process the login request */
-router.post('/login', function(req, res, next) 
-{
-  passport.authenticate('local', function(err, user, info)
-  {
-    // are there serer errors?
-    if(err)
-    {
-      console.error(err);
-      res.end(err);
-    }
-    
-    // are there login errors?
-    if(!user)
-    {
-      req.flash('loginMessage', 'Authentication Error');
-      return res.redirect('/login');
-    }
-
-    req.logIn(user, function(err)
-    {
-      // are there db errors?
-      if(err)
-      {
-        console.error(err);
-        res.end(err);
-      }
-
-      return res.redirect('/contact-list')
-    });
-  })(req, res, next);
-});
+router.post('/login', ProcessLoginPage);
 
 /* GET - Display register page. */
-router.get('/register', function(req, res, next) 
-{
-  if(!req.user)
-  {
-  return res.render('index', 
-    { title: 'Register', page: 'register', messages: req.flash('registerMessage'), displayName: UserDisplayName(req) });
-  }
-  return res.redirect('/contact-list');
-});
+router.get('/register', DisplayRegisterPage);
 
 /* Process the register request */
-router.post('/register', function(req, res, next) 
-{
-  // instantiate a new user object
-  let newUser = new User
-  ({
-    username: req.body.username,
-    EmailAddress: req.body.emailAddress,
-    DisplayName: req.body.firstName + " " + req.body.lastName
-  });
-
-  User.register(newUser, req.body.password, function(err)
-  {
-    if(err)
-    {
-      if(err.name == "UserExistsError")
-      {
-        console.error('ERROR: Inserting User');
-        req.flash('registerMessage', 'Registration Error');
-        console.error('ERROR: User Already Exists');
-      }
-      req.flash('registerMessage', 'Server Failure');
-      console.error(err.name);
-      return res.redirect('/register');
-    }
-    
-    // automatically login the user
-    return passport.authenticate('local')(req, res, ()=>
-    {
-      return res.redirect('/contact-list');
-    });
-  });
-});
+router.post('/register', ProcessRegisterPage);
 
 /* process logout request */
-router.get('/logout', function(req, res, next) 
-{
-  req.logOut();
-
-  res.redirect('/login');
-});
+router.get('/logout', ProcessLogoutPage);
 
 export default router;
