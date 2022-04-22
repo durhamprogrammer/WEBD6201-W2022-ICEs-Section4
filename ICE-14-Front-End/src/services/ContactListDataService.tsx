db0de867fa6d27e2a6f93858d3fb3f6e04d28c5d
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2022-04-22 15:29:48 -0400

    updated ContactListDataService

--- ICE-14-Front-End/src/services/ContactListDataService.tsx
+++ ICE-14-Front-End/src/services/ContactListDataService.tsx
@@ -1,30 +1,31 @@
 import http from '../http-common';
 import IContactData from '../models/Contact';
+import AuthHeader from './AuthHeader';
 class ContactListDataService
 {
     create(data: IContactData)
     {
-        return http.post<IContactData>("/add", data);
+        return http.post<IContactData>("/add", data, AuthHeader());
     }
 
     readAll()
     {
-        return http.get<Array<IContactData>>("/contact-list");
+        return http.get<Array<IContactData>>("/contact-list", AuthHeader());
     }
 
     readOne(id: any)
     {
-        return http.get<IContactData>(`/edit/${id}`);
+        return http.get<IContactData>(`/edit/${id}`, AuthHeader());
     }
 
     update(data: IContactData, id: any)
     {
-        return http.post<IContactData>(`/edit/${id}`, data);
+        return http.post<IContactData>(`/edit/${id}`, data, AuthHeader());
     }
 
     delete(id: any)
     {
-        return http.get<IContactData>(`/delete/${id}`);
+        return http.get<IContactData>(`/delete/${id}`, AuthHeader());
     }
 }
 
