909b7bdbc0cf14bd2612268f29aa8b5f08bf1d8f
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2022-04-22 15:27:16 -0400

    updated AuthHeader service

--- ICE-14-Front-End/src/services/AuthHeader.tsx
+++ ICE-14-Front-End/src/services/AuthHeader.tsx
@@ -1,11 +1,18 @@
-export default function authHeader() {
+export default function AuthHeader() {
     const userStr = localStorage.getItem("user");
     let user = null;
+
     if (userStr)
+    {
       user = JSON.parse(userStr);
-    if (user && user.token) {
-      return { Authorization: 'Bearer ' + user.token };
-    } else {
+    }
+      
+    if (user && user.token) 
+    {
+      return {headers: {"Authorization": "Bearer " + user.token }};
+    } 
+    else 
+    {
       return {};
     }
   }
